cfa8bcafbdecf0249f5ed2895c2b8b023744b5b7
Casey Dreier <cdreier@gmail.com>
2011-05-07 16:14:30 -0400

    Updating regex for finding closing </head> element. Making most methods private. Updating tests and adding test to ensure that only text/html triggers this behavior.

--- test/unscriptulous_test.rb
+++ test/unscriptulous_test.rb
@@ -7,11 +7,11 @@ class UnscriptulousTest < Test::Unit::TestCase
   include Rack::Test::Methods
 
   def app
-    @other_app =  Proc.new do |env|
+    @other_app ||= Proc.new do |env|
       [
         200,
-        {'Content-Type' =>  'text/html', 'Content-Length' => '66'},
-        Rack::Response.new(["<html><head></head><body><p>The Bucket of Truth</p></body></html>"])
+        {'Content-Type' =>  'text/html', 'Content-Length' => test_page_html.length.to_s},
+        Rack::Response.new(test_page_html)
       ]
     end
     Rack::Unscriptulous.new(@other_app)
@@ -24,8 +24,28 @@ class UnscriptulousTest < Test::Unit::TestCase
 
   def test_content_length_is_increased
     get '/'
-    expected = app.inline_code.length + app.no_javascript_warning.length + 66
+    expected = app.send(:inline_code).length + app.send(:no_javascript_warning).length + test_page_html.length
     assert_equal expected, last_response.headers['Content-Length'].to_i
+  end
+
+  def test_takes_no_action_on_non_html_content_type
+    json = "{\"success\":\"success\"}"
+    @other_app =  Proc.new do |env|
+        [
+          200,
+          {'Content-Type' =>  'application/json', 'Content-Length' => json.length.to_s},
+          Rack::Response.new(json)
+        ]
+      end
+    get '/'
+    assert_equal json.length, last_response.headers['Content-Length'].to_i
+    assert_equal json, last_response.body
+  end
+
+  private
+
+  def test_page_html
+    '<html><head></head><body class="such-and-such"><p>The Bucket of Truth</p></body></html>'
   end
 
 end
--- unscriptulous.rb
+++ unscriptulous.rb
@@ -8,21 +8,24 @@ module Rack
       @status, @headers, @response = @app.call(env)
       if @headers["Content-Type"].respond_to?(:include?) && @headers["Content-Type"].include?("text/html")
         @response.each do |response_line|
-          insert_js(response_line)   if response_line =~ /<\/head>/
+          insert_js(response_line)   if response_line =~ head_regex
           insert_html(response_line) if response_line =~ body_regex
         end
       end
       [@status, @headers, @response]
     end
 
+  private
+
     def insert_js(response_line)
       add_to_content_length(inline_code.length)
-      response_line.sub!(/<\/head>/, "#{inline_code}\n</head>")
+      response_line.sub!(head_regex, inline_code + '\1')
     end
 
+    # Inserts HTML of javascript warning after the opening body tag.
     def insert_html(response_line)
       add_to_content_length(no_javascript_warning.length)
-      response_line.sub!(body_regex, '\1'+"\n#{no_javascript_warning}")
+      response_line.sub!(body_regex, '\1'+ no_javascript_warning)
     end
 
     def no_javascript_warning
@@ -36,7 +39,11 @@ module Rack
     end
 
     def body_regex
-      /(<\s*body[^>]*>)/
+      /(<\s*body[^>]*>)/i
+    end
+
+    def head_regex
+      /(<\s*\/head[^>]*>)/i
     end
 
     def add_to_content_length(number)
